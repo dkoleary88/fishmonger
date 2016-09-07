8fd6ed25b928a38c5eab9c681ac257e0d6dc1500
dkoleary88 <dan@dko.io>
2016-09-06 22:58:43 -0700

    Fix file reference errors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 const _ = require('lodash');
 const fs = require('fs');
-const Exchange = require('./exchange');
+const path = require('path');
+const Exchange = require(path.join(__dirname, '/exchange'));
 
-let configs = fs.readdirSync('vendors');
+let configs = fs.readdirSync(path.join(__dirname, 'vendors'));
 for (let filename of configs) {
   if (!/\.json$/.test(filename)) continue;
   let name = filename.split('.')[0];
-  let config = require(`./vendors/${filename}`);
+  let config = require(path.join(__dirname, `/vendors/${filename}`));
   exports[name] = new Exchange(config);
 }
